0f7b5ce5e19c1ad4c22b9fc8d385833f5707212f
TautvydasVaitkus <tautvydas97@gmail.com>
2022-03-05 15:09:48 +0200

    A

--- Assets/Scripts/ZombieMove.cs
+++ Assets/Scripts/ZombieMove.cs
@@ -12,7 +12,7 @@ public class ZombieMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position.Set(0, 0, -12);
+        transform.position.Set(26, 0.5f, -65);
         anim = gameObject.GetComponent<Animation>();
         m_Rigidbody = GetComponent<Rigidbody>();
         timer = FindObjectOfType<Timer>();
@@ -24,18 +24,15 @@ public class ZombieMove : MonoBehaviour
     void Update()
     {
 
-        if (timer.timeLeft >= 0)
+        /*if (timer.timeLeft >= 0)
         {
             anim.Play("Zombie|ZombieIdle");
-        }  
+        }*/  
         
-        if (timer.timeLeft <= 0)
-        {
-            if (transform.position.z <= 6)
+            if (transform.position.z <= -44)
             {
                 transform.Translate(Vector3.forward * speed * Time.deltaTime);
                 anim.Play("Zombie|ZombieWalk");
-            }
-        }                  
+            }                 
     }
 }
