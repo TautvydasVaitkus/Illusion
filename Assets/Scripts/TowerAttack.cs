c7b13745aeee59b49504b2fdb7b6ed1a92feb218
TautvydasVaitkus <tautvydas97@gmail.com>
2022-03-06 11:03:05 +0200

    a

--- Assets/Scripts/TowerAttack.cs
+++ Assets/Scripts/TowerAttack.cs
@@ -6,19 +6,49 @@ public class TowerAttack : MonoBehaviour
 {
 
     public ZombieMove movingZombie;
+    private float nextActionTime = -5f;
+    public GameObject findZombie;
+    public SpawnArmy zombieClones;
+    private float minDist;
+    private GameObject bestTarget;
+    private AudioSource source;
 
     // Start is called before the first frame update
     void Start()
     {
         movingZombie = FindObjectOfType<ZombieMove>();
+        findZombie = GameObject.Find("ZombieMenu");
+        zombieClones = FindObjectOfType<SpawnArmy>();
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z - movingZombie.transform.position.z < 2)
+        minDist = 999;
+
+
+        if (nextActionTime < 0f)
         {
-            Destroy(movingZombie.gameObject);
+            Debug.Log("SHOT");
+            foreach (var cloneObj in zombieClones._clones)
+            {
+                if (transform.position.z - cloneObj.transform.position.z < 0.02f && transform.position.y - cloneObj.transform.position.y < 2)
+                {
+                    Destroy(cloneObj);
+                }
+            }
+            nextActionTime = 5f;
         }
+        else
+        {
+            nextActionTime -= Time.deltaTime;
+        }
+ 
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSecondsRealtime(5f);
     }
 }
